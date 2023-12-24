eb11a98b69f76d793b80c49112f6a23a461d517a
anakeanes <anakeanes.h@gmail.com>
2023-12-24 20:06:21 +0700

    create initmysql to index.js, make it async function and have the await. config the conn function details

--- test-docker/index.js
+++ test-docker/index.js
@@ -7,13 +7,26 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+let conn = null
+
+// initMySQL is to initiate mysql, and then define at line 29
+const initMySQL = async () => {
+    conn = await mysql.createConnection({ 
+        host: 'db', // or it can be localhost, same as in db in docker-compose.yml
+        user: 'root',
+        password: 'root',
+        database: 'tutorial'
+    })  
+}
+
 //create API hello-world
 
 app.get ('/hello-world', (req, res) => { //go to path http://localhost:8000/hello-world
     res.send('hello world')
 })
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    await initMySQL()
     console.log(`Server running at http://localhost:${port}/`) //beware of using '' and ``
 })
 
